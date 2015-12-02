b8e4e6e408b668b636a3a6288c849581db830cf6
Felipe Borges <jfpsb_felipe2@hotmail.com>
2015-12-02 12:25:05 -0300

    Tab para mostrar letra original

--- WebContent/resources/js/letramusica.js
+++ WebContent/resources/js/letramusica.js
@@ -3,18 +3,31 @@ $.ajax({
   //other options...
 });
 
-function loadDoc(musica) {
+function loadTraducao(musica) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
-      document.getElementById("textArea").innerHTML = xhttp.responseText;
+      document.getElementById("tradTextArea").innerHTML = xhttp.responseText;
     }
   };
   
-  xhttp.open("GET", "letra/" + musica + ".txt", true);
+  xhttp.open("GET", "letra/traducao/" + musica + ".txt", true);
   xhttp.send();
 }
 
+function loadOriginal(musica) {
+	  var xhttp = new XMLHttpRequest();
+	  xhttp.onreadystatechange = function() {
+	    if (xhttp.readyState == 4 && xhttp.status == 200) {
+	      document.getElementById("origTextArea").innerHTML = xhttp.responseText;
+	    }
+	  };
+	  
+	  xhttp.open("GET", "letra/original/" + musica + ".txt", true);
+	  xhttp.send();
+	}
+
 function onDialogClose() {
-	document.getElementById("textArea").innerHTML = "<p>Aguarde...</p>";
+	document.getElementById("tradTextArea").innerHTML = "<p>Aguarde...</p>";
+	document.getElementById("origTextArea").innerHTML = "<p>Aguarde...</p>";
 }
